a126d2856b537b44c1ef88f6ac3a830277d7872c
Hugo Brandao <hugobrandao.dev@gmail.com>
2026-08-05 14:23:46 -0400

    Add tipo boolean

---
+++ modulo001/aula010/booleanType.ts
@@ -0,0 +1,13 @@
+let tarefaConcluida: boolean = true;
+let tarefaPendente: boolean = false;
+
+console.log(tarefaConcluida);
+console.log(tarefaPendente);
+
+let concluido: boolean = false;
+
+if (concluido) {
+    console.log("Tarefa concluída com sucesso!!");
+} else {
+    console.log("Tarefa pendente.");
+}
