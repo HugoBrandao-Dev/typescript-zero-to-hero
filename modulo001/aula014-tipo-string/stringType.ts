a4b8d72f1b3ec70a5b85b7226a81d1bfed68e41a
Hugo Brandao <hugobrandao.dev@gmail.com>
2026-08-07 14:35:28 -0400

    Add string

---
+++ modulo001/aula014-tipo-string/stringType.ts
@@ -0,0 +1,8 @@
+let nome: string = "Hugo";
+let sobrenome: string = 'Brandão';
+let linguagemProgramacao = `TypeScript`;
+
+console.log(`
+    Nome completo: ${ nome } ${ sobrenome }
+    Linguagem de Programação: ${ linguagemProgramacao }
+    `)
